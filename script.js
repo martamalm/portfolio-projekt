a96f8b47dbb01ef71b001f4c5034a6b56dfe5d2c
martamalm <144331091+martamalm@users.noreply.github.com>
2023-12-12 12:22:45 +0100

    fix med responsiviteten

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ const tabcontents = document.getElementsByClassName("tab-contents");
         event.currentTarget.classList.add("active-link");
         document.getElementById(tabname).classList.add("active-tab");
     }
+
+    const sidemenu = document.getElementById("sidemenu");
+
+    function openmenu(){
+        sidemenu.style.right = "0";
+    }
+
+    function closemenu(){
+        sidemenu.style.right = "-200px";
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Get the button element
+        var scrollToTopBtn = document.getElementById("scrollToTop");
+    
+        // Show/hide the button based on the scroll position
+        window.onscroll = function() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                scrollToTopBtn.style.display = "block";
+            } else {
+                scrollToTopBtn.style.display = "none";
+            }
+        };
+    
+        // Scroll to the top when the button is clicked
+        scrollToTopBtn.addEventListener("click", function() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+        });
+    });
